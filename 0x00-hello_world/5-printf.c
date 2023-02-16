abc4f2f973411a2e3e0beb22d438671ddd72c3b3
pappiah1993 <appiahpeter06@gmail.com>
2023-02-16 14:58:02 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - represent the main function of the task
+ *
+ * Return: is successful when 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
