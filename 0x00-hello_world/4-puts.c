f9b6806ad041d9365d13f06b4c7be56ee967c1a0
pappiah1993 <appiahpeter06@gmail.com>
2023-02-16 14:02:15 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzzle);
+	return (0):
+}
+:%s/\s\+$//e
