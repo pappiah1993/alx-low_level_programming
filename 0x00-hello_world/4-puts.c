eda4a22586ad2c25c119ce8e8df70caf691c830c
pappiah1993 <appiahpeter06@gmail.com>
2023-02-16 14:22:41 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,6 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzzle);
-	return (0):
+	puts("\"Programming is like building a multilingual puzzzle");
+	return (0);
 }
-:%s/\s\+$//e
